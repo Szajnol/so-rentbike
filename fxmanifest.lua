13f19e0d20182624ccc3d027e90700efff7db151
SOK3Y (sokey) <126024643+SOK3Y@users.noreply.github.com>
2023-07-30 02:04:36 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,10 @@
 fx_version "cerulean"
 game "gta5"
 lua54 "yes"
+author 'SO INDUSTRIES - SOK3Y'
 
 client_script "client.lua"
 
 server_script "server.lua"
+
+shared_script '@es_extended/imports.lua'

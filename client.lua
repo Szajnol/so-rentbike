8fca8be922f31fb7b4c0d22278cb7bb9d625ab53
SOK3Y (sokey) <126024643+SOK3Y@users.noreply.github.com>
2023-08-02 00:38:04 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,23 +1,43 @@
 ESX = exports["es_extended"]:getSharedObject()
 
--- blipy
-local bliprent = {
-    {title="Wypożyczalnia Rowerów", colour=0, id=226, x = 197.0491, y = -930.7761, z = 30.6864}, 
+BikeRentPeds = {
+    {x = 232.56,  y = -859.44,  z = 29.92-1, h = 163.76},
+    {x = 245.92,  y = -580.68,  z = 43.24-1, h = 72.08},
+    {x = -504.68,  y = -256.68,  z = 35.68-1, h = 212.44},
+    {x = -1808.6,  y = -1189.52,  z = 13.0-1, h = 233.76},
+    {x = -820.56,  y = -1214.12,  z = 6.92-1, h = 48.24},
+    {x = -234.84,  y = -978.52,  z = 29.28-1, h = 74.24},
+    {x = 2011.32,  y = 3765.76,  z = 32.2-1, h = 119.48},
+    {x = 122.76,  y = 6406.36,  z = 31.36-1, h = 315.76},
 }
 
 Citizen.CreateThread(function()
-     for _, info in pairs(bliprent) do
-       info.blip = AddBlipForCoord(info.x, info.y, info.z)
-       SetBlipSprite(info.blip, info.id)
-       SetBlipDisplay(info.blip, 4)
-       SetBlipScale(info.blip, 0.7)
-       SetBlipColour(info.blip, info.colour)
-       SetBlipAsShortRange(info.blip, true)
-       BeginTextCommandSetBlipName("STRING")
-       AddTextComponentString(info.title)
-       EndTextCommandSetBlipName(info.blip)
-       Citizen.Wait(0)
-     end
+	for i,v in pairs(BikeRentPeds) do
+		local blip = AddBlipForCoord(v.x,v.y,v.z)
+
+		SetBlipSprite (blip, 226)
+		SetBlipDisplay(blip, 4)
+		SetBlipScale  (blip, 0.7)
+		SetBlipColour (blip, 0)
+		SetBlipAsShortRange(blip, true)
+
+		BeginTextCommandSetBlipName('STRING')
+		AddTextComponentSubstringPlayerName("Wypożyczalnia rowerów")
+		EndTextCommandSetBlipName(blip)
+
+
+		modelHash = `a_m_y_epsilon_01`
+		RequestModel(modelHash)
+		while not HasModelLoaded(modelHash) do
+			Wait(1)
+		end
+		local thisPed = CreatePed(26, modelHash, v.x, v.y, v.z, v.h, false, false)
+		Citizen.Wait(100)
+		SetEntityAsMissionEntity(thisPed, true, true)
+		FreezeEntityPosition(thisPed, true)
+		SetEntityInvincible(thisPed, true)
+		TaskSetBlockingOfNonTemporaryEvents(thisPed, true)
+	end
 end)
 
 
@@ -38,13 +58,5 @@ Citizen.CreateThread(function()
     },
     distance = 2
   })
-
-  RequestModel(GetHashKey("a_f_o_soucent_01"))
-  while not HasModelLoaded(GetHashKey("a_f_o_soucent_01")) do
-    Wait(10)
-  end
-
-  local Ped = CreatePed(0, "a_f_o_soucent_01", 197.0491, -930.7761, 30.6864, 328.9742, false, true)
-  FreezeEntityPosition(Ped, true)
 end)
-  
+
